43dc5d55cca410d08786f4c0283c2053c847f58e
Daniel Pereira <danielp6r@gmail.com>
2025-04-02 10:31:23 -0300

    update aula 01

--- java-lambdas-streams-spring-framework/Aula 01/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ java-lambdas-streams-spring-framework/Aula 01/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +17,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=lost&Season=1&apikey=a1547706");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=lost&apikey=a1547706");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
