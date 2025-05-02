f2b714655e475d576ff1dfff09af6e170776ff29
Daniel Pereira <danielp6r@gmail.com>
2025-05-02 10:48:14 -0300

    update aula 05

---
+++ java-criando-primeira-api-conectando-front/Aula 05/screenmatch-frases/src/main/java/br/com/alura/screenmatch_frases/service/FraseService.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatch_frases.service;
+
+import br.com.alura.screenmatch_frases.dto.FraseDTO;
+import br.com.alura.screenmatch_frases.model.Frase;
+import br.com.alura.screenmatch_frases.repository.FraseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FraseService {
+    @Autowired
+    FraseRepository repositorio;
+
+    public FraseDTO obterFraseAleatoria() {
+        Frase frase = repositorio.buscaFraseAleatoria();
+        return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonagem(), frase.getPoster());
+    }
+}
