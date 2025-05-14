63cee3f23bcd4d82d8d063e14105385f6334bb38
Daniel Pereira <danielp6r@gmail.com>
2025-05-14 09:42:55 -0300

    update aula 04
        Escrever testes automatizados em uma aplicação com Spring Boot;
        Escrever testes automatizados de uma interface Repository, seguindo a estratégia de usar o mesmo banco de dados que a aplicação utiliza;
        Sobrescrever propriedades do arquivo application.properties, criando outro arquivo chamado application-test.properties que seja carregado apenas ao executar os testes, utilizando para isso a anotação @ActiveProfiles;
        Escrever testes automatizados de uma classe Controller, utilizando a classe MockMvc para simular requisições na API;
        Testar cenários de erro 400 e código 200 no teste de uma classe controller.

---
+++ spring-boot-3-documente-teste-prepare-api-deploy/Aula 04/api/src/main/java/med/voll/api/model/Consulta.java
@@ -0,0 +1,47 @@
+package med.voll.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
+
+    @Enumerated(EnumType.STRING)
+    private MotivoCancelamento motivoCancelamento;
+
+    public Consulta(Long id, Medico medico, Paciente paciente, LocalDateTime data) {
+        this.id = id;
+        this.medico = medico;
+        this.paciente = paciente;
+        this.data = data;
+    }
+
+    public void cancelar(String motivo) {
+        this.motivoCancelamento = MotivoCancelamento.valueOf(motivo);
+    }
+
+}
