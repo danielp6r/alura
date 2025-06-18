f6bf6f61346381144e8897368500af53842c8a03
Daniel Pereira <danielp6r@gmail.com>
2025-06-18 11:14:09 -0300

    update aula 03 - incompleta
    Refazer essa aula...

---
+++ java-gestao-memoria-crie-aplicacoes-performaticas-robustas/Aula 03/gestao-memoria-vollmed/src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -0,0 +1,67 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
+
+    @Column(name = "motivo_cancelamento")
+    @Enumerated(EnumType.STRING)
+    private MotivoCancelamento motivoCancelamento;
+
+    public void cancelar(MotivoCancelamento motivo) {
+        this.motivoCancelamento = motivo;
+    }
+
+    @Override
+    public String toString() {
+        return "Consulta{" +
+                "medico=" + medico +
+                ", paciente=" + paciente +
+                ", data=" + data +
+                '}';
+    }
+
+    //As demais classes model serão anotadas com @EqualsAndHashCode(of="id")
+    //Nesse caso, para fins de exemplificação, vou deixar criado os métodos na sequencia:
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Consulta consulta = (Consulta) o;
+        return Objects.equals(id, consulta.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
